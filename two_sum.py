515360462e1109792a7b977abeb3b37a3dc8067f
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-03-23 19:32:20 -0500

    Leet code

--- two_sum.py
+++ two_sum.py
@@ -5,22 +5,19 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 '''
 
-numbers = 5
 
-def check(target, record):
-    for num in range(len(record)-1):
-        for x in range(1,len(record)):
-            sum = record[num] + record[x]
-            if sum == target:
-                return num,x
+class Solution(object):
 
-def main():
-    record = list()
-    for num in range(numbers):
-        number_new = int(input('Enter the number: '))
-        record.append(number_new)
-    target = int(input('Enter the target sum: '))
-    print('The index of the numbers which add up to the target sum are:',(check(target, record)))
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
 
-if __name__ == "__main__":
-    main()
+        temp = {}
+        for index, elem in enumerate(nums):
+            if elem in temp:
+                return [temp[elem], index]
+            else:
+                temp[target - elem] = index
