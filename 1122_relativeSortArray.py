f3b7aa0fa017c158efbd70c81c8d011075bfe767
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-09-25 22:30:25 -0500

    leet code

---
+++ 1122_relativeSortArray.py
@@ -0,0 +1,24 @@
+#relative sort array
+
+'''
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
+'''
+
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        result = list()
+        for each in arr2:
+            while each in arr1:
+                result.append(each)
+                arr1.remove(each)
+        arr1.sort()
+        result.extend(arr1)
+        return result
