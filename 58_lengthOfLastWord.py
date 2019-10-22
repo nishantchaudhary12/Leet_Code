45d59b0dbfecefa9dc68c14844418f3a88115fff
Nishant Chaudhary <34044056+nishantchaudhary12@users.noreply.github.com>
2019-10-21 23:26:19 -0500

    leet code

---
+++ 58_lengthOfLastWord.py
@@ -0,0 +1,20 @@
+'''Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.'''
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s:
+            s = s.strip()
+            s_list = s.split(' ')
+            length = len(s_list[-1])
+        else:
+            length = 0
+        return length
