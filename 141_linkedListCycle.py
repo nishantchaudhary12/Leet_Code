3bb7a584b9d2a6e3ca6cec45d247b4b8fc497a4b
Nishant Chaudhary <34044056+nishantchaudhary12@users.noreply.github.com>
2019-12-24 14:55:14 -0600

    leet code

---
+++ 141_linkedListCycle.py
@@ -0,0 +1,21 @@
+'''Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the
+linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        temp = head
+        while temp:
+            if temp.val == '0':
+                return True
+            temp.val = '0'
+            temp = temp.next
+        return False
