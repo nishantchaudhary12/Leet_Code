c93be32ecf8b6f44b79ce3a077e37ed54e695509
Nishant Chaudhary <34044056+nishantchaudhary12@users.noreply.github.com>
2020-01-18 00:51:27 -0600

    leet code

---
+++ 157_readNCharactersGivenRead4.py
@@ -0,0 +1,47 @@
+'''Given a file and assume that you can only read the file using a given method read4, implement a method to read n characters.
+
+
+
+Method read4:
+
+The API read4 reads 4 consecutive characters from the file, then writes those characters into the buffer array buf.
+
+The return value is the number of actual characters read.
+
+Note that read4() has its own file pointer, much like FILE *fp in C.'''
+
+"""
+The read4 API is already defined for you.
+
+    @param buf, a list of characters
+    @return an integer
+    def read4(buf):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        buf4 = [''] * 4
+        count = 0
+        while n > 0:
+            read = min(n, read4(buf4))
+            if read == 0:
+                break
+            buf[count:count + read] = buf4[:read]
+            count += read
+            n -= read
+        return count
+
+
