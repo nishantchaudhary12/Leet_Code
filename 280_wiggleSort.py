10941219b031ee7d6f4922261a046778ffd149da
Nishant Chaudhary <34044056+nishantchaudhary12@users.noreply.github.com>
2020-01-22 12:48:59 -0600

    leet code

---
+++ 280_wiggleSort.py
@@ -0,0 +1,17 @@
+'''Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....'''
+
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums) - 1):
+            if i % 2 == 0:
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            else:
+                if nums[i] < nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+
+
