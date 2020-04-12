2e50f5c47b15efe08001e8ca255c0331b30e5cb8
Nishant Chaudhary <34044056+nishantchaudhary12@users.noreply.github.com>
2020-04-12 16:24:59 -0700

    leet code

---
+++ Java/412_fizzBuzz.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> arr = new ArrayList<String>();
+        for(int i=1; i<=n; i++){
+            if(i%5 == 0 && i%3 == 0){
+                arr.add("FizzBuzz");
+            }else if(i%3 == 0){
+                arr.add("Fizz");
+            }else if(i%5 == 0){
+                arr.add("Buzz");
+            }else{
+                arr.add(String.valueOf(i));
+            }
+        }
+        return arr;
+    }
+}
